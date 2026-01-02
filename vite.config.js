a1b7f9719e28d20caf0ec7f2c14f47b19aa0acca
Chandru <ck6582293@gmail.com>
2026-01-03 00:05:24 +0530

    sec  commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/establishing_a_high_tech_crime_response_system/',
   plugins: [react(),tailwindcss(),],
 })
